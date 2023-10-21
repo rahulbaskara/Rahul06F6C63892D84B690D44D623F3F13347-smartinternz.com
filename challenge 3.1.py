0af947b410e7da11735dc292482c1637809c50b6
rahulbaskara <148629675+rahulbaskara@users.noreply.github.com>
2023-10-21 18:00:45 +0530

    Add files via upload

---
+++ challenge 3.1.py
@@ -0,0 +1,17 @@
+def
+LinearSearchProduct(productList, targetProduct):
+indices - [J
+for Index, product in enumerate(productList):
+1f
+product a targetProduct:
+Indices. append( index)
+return indices I
+21
+22
+• Example usage! products
+- ["shoes"
+'boot", "loafer", "shoes",
+"sandal",
+*shoes "] target -
+"shoes"
+result * LinearSearchProduct(products, target)
